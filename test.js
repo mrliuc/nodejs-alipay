2a5d3592550353d0cd24c5962175d2096ab1070b
mrliuc <mrliuc@qq.com>
2022-01-10 11:33:26 +0800

    20220110 增加查询主账户余额接口

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-var fs = require('fs');
-var Alipay = require('./index');
+const fs = require('fs');
+const Alipay = require('./index');
 
-var pay = new Alipay({
+const pay = new Alipay({
     rquestUrl: 'https://openapi.alipaydev.com/gateway.do',
     app_id: 'XXXXXXX',
     return_url: 'http://XXXXX/alipay_return',
     notify_url: 'http://XXXXX/bl/alipay_notify',
-    app_private_key: fs.readFileSync('./pem/private_2048.txt'), //支付宝商户应用私钥
+    app_private_key: fs.readFileSync('./pem/private_2048.txt'), // 支付宝商户应用私钥
     alipay_public_key: fs.readFileSync('./pem/public_2048.txt'),
 });
 
-var paramStr = pay.buildSignOrderParam({
+const paramStr = pay.buildSignOrderParam({
     boby: '余额充值',
     subject: '余额充值',
     out_trade_no: new Date().getTime(),
     total_amount: 1,
-    product_code: 'FAST_INSTANT_TRADE_PAY'
+    product_code: 'FAST_INSTANT_TRADE_PAY',
 });
 
-console.log(paramStr)
+console.log(paramStr);
